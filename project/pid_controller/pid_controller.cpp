c79a1854f1e1ac9e6a982eb62453cbef0b065165
root <mathilde.badoual@gmail.com>
2021-03-13 16:01:52 +0000

    solve errors

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -33,6 +33,7 @@ double PID::TotalError() {
    * TODO: Calculate and return the total error
     * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
    */
+    double control;
     return control;
 }
 
@@ -40,5 +41,4 @@ double PID::UpdateDeltaTime(double new_delta_time) {
    /**
    * TODO: Update the delta time with new value
    */
-    return delta_time;
 }
